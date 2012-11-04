ca6b98a3aa67eed1e7ef55ead41e7cc74d003acd
horviainen@gmail.com <mosse44>
2012-11-04 23:19:36 +0200

    -

--- ReittihakuA/src/ReittihakuA.java
+++ ReittihakuA/src/ReittihakuA.java
@@ -1,11 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.HashMap;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 
 /**
  *
- * @author anne
+ * @author Anne Hankosalo
  */
 public class ReittihakuA {
 
